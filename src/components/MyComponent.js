aeb36bfa1f740db02eed9449df23fd280e698602
kenzongo <overdev.khang@gmail.com>
2024-03-07 19:35:50 +0700

    video 17

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,9 +1,18 @@
 import React from "react";
 class MyComponent extends React.Component {
     //JSX
+    state = {
+        name: 'Kenzongo',
+        age: 24,
+        address: 'Ho Chi Minh City'
+    }
     render() {
         return (
-            <div>My first Component</div>
+            <div>
+                <p>
+                    My name is {this.state.name}, {this.state.age} years old &amp; live in {this.state.address}
+                </p>
+            </div>
         );
     }
 }

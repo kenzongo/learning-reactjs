89dbfa03fd2b3a99c2cb4d40496d4c5b91e1c502
kenzongo <overdev.khang@gmail.com>
2024-03-07 19:12:35 +0700

    video 16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+class MyComponent extends React.Component {
+    //JSX
+    render() {
+        return (
+            <div>My first Component</div>
+        );
+    }
+}
+export default MyComponent;

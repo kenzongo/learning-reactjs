46fdb38d387a60ba129a00868ce87c40f346753a
kenzongo <overdev.khang@gmail.com>
2024-03-07 21:29:31 +0700

    video 20

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,17 +1,34 @@
 import React from "react";
 class MyComponent extends React.Component {
     //JSX
+    //Solution 1:
+    // constructor(props) {
+    //     super(props)
+    //     this.state = {
+    //         name: 'Kenzongo',
+    //         age: 24,
+    //         address: 'Ho Chi Minh City'
+    //     }
+    //     this.handleClick = this.handleClick.bind(this)
+    //     this.handleMouseOver = this.handleMouseOver.bind(this)
+    // }
+    //Solution 2
     state = {
         name: 'Kenzongo',
         age: 24,
         address: 'Ho Chi Minh City'
     }
     handleMouseOver(event) {
-        console.log(event.pageX);
+        //console.log(event.pageX);
     }
     handleClick(event) {
         console.log("You just click me!!");
         console.log(event.target);
+        //console.log('My name is ', this.state.name);
+        this.setState({
+            name: 'Khang',
+            age: Math.floor(Math.random() * 100 + 1)
+        })
     }
     render() {
         return (
@@ -20,7 +37,7 @@ class MyComponent extends React.Component {
                     My name is {this.state.name}, {this.state.age} years old &amp; live in {this.state.address}
                 </p>
                 <button onMouseOver={this.handleMouseOver}>Over me</button>
-                <button onClick={this.handleClick}>Click me</button>
+                <button onClick={(event) => this.handleClick(event)}>Click me</button>
             </div>
         );
     }

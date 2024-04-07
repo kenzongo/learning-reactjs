a935c417edae16c3469f2e7e15c87d67771e8c39
kenzongo <overdev.khang@gmail.com>
2024-04-08 03:37:43 +0700

    video 53

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,12 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+//import NavDropdown from 'react-bootstrap/NavDropdown';
 import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <Navbar bg="dark" expand="lg" className="bg-body-tertiary" variant="dark">
+        <Navbar bg="light" expand="lg" className="bg-body-tertiary" variant="light">
             <Container>
                 <NavLink to={`/`} className="navbar-brand">React-Bootstrap</NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -20,11 +20,13 @@ const Header = () => {
                         <Nav.NavLink href="admins">Admin</Nav.NavLink> */}
                     </Nav>
                     <Nav>
-                        <NavDropdown title="Settings" id="basic-nav-dropdown">
+                        <button className="btn-login">Log in</button>
+                        <button className="btn-signup">Sign up</button>
+                        {/* <NavDropdown title="Settings" id="basic-nav-dropdown">
                             <NavDropdown.Item>Log in</NavDropdown.Item>
                             <NavDropdown.Item>Log out</NavDropdown.Item>
                             <NavDropdown.Item>User Info</NavDropdown.Item>
-                        </NavDropdown>
+                        </NavDropdown> */}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -3,10 +3,27 @@ import videoMp4 from '../../assets/hero.mp4';
 const HomePage = (props) => {
     return (
         <div className="homepage-container">
-            <video autoPlay muted loop>
-                <source data-testid="currentMp4Video" src={videoMp4} type="video/mp4" />
-                <source data-testid="currentWebmVideo" src={videoWebm} type="video/webm" />
-            </video>
+            <div className="homepage-video">
+                <video autoPlay muted loop width="100%">
+                    <source data-testid="currentMp4Video" src={videoMp4} type="video/mp4" />
+                    <source data-testid="currentWebmVideo" src={videoWebm} type="video/webm" />
+                </video>
+            </div>
+            <div className="homepage-content">
+                <h1 className="title-main">
+                    Make forms
+                    worth filling out
+                </h1>
+                <div className="title-detail">
+                    Get more data—like signups,
+                    feedback, and anything else—with forms
+                    designed to be <span style={{ fontWeight: "bold" }}>refreshingly different.</span></div>
+                <div>
+                    <button className="title-button">
+                        Get started—it's free
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }

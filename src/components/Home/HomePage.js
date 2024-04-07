5b8e61eb1b167646935469a179f846650474e312
kenzongo <overdev.khang@gmail.com>
2024-04-07 14:41:27 +0700

    video 52

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,7 +1,12 @@
+import videoWebm from '../../assets/hero.webm';
+import videoMp4 from '../../assets/hero.mp4';
 const HomePage = (props) => {
     return (
         <div className="homepage-container">
-            HomePage Container
+            <video autoPlay muted loop>
+                <source data-testid="currentMp4Video" src={videoMp4} type="video/mp4" />
+                <source data-testid="currentWebmVideo" src={videoWebm} type="video/webm" />
+            </video>
         </div>
     )
 }
